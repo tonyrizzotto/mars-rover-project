5ac3b698433daf6d6483812f906d1cd56705b364
Tony Rizzotto <anthonyrizzotto@gmail.com>
2021-08-17 09:35:47 -0400

    Add Feature: Image Display
    Built up a barebones image slider that cycles through the returned results.
    
    TODO: The component re-renders each time the activeIndex is changed. Need to fix that, but also make sure the image sliders don't show during the initial render.

--- src/components/DisplayPhotos.js
+++ src/components/DisplayPhotos.js
@@ -1,20 +1,64 @@
 import React from 'react';
+import './DisplayPhotos.css';
 
 class DisplayPhotos extends React.Component {
+  state = {
+    activeIndex: 0,
+  };
+
+  // showPhotos = () => {
+  //   if (this.props.photos === undefined) {
+  //     console.log(this.props.photos);
+  //     return;
+  //   } else {
+  //     let currentPhoto = this.props.photos[this.state.activeIndex];
+  //     console.log(currentPhoto);
+  //   }
+  // };
+
   render() {
     return (
       <div>
-        {this.props.photos.map((photo) => {
-          console.log(photo);
-          return (
-            <img
-              src={`${photo.img_src}`}
-              className="photo"
-              key={photo.id}
-              alt={`Image taken on ${photo.earth_date}`}
-            />
-          );
+        {this.props.photos.map((photo, index) => {
+          if (index === this.state.activeIndex) {
+            return (
+              <img
+                src={`${photo.img_src}`}
+                className={`photo
+                    ${index === this.state.activeIndex ? 'active' : 'inactive'}
+                  `}
+                key={photo.id}
+                alt={`taken on ${photo.earth_date}`}
+              />
+            );
+          }
         })}
+        <button
+          onClick={() => {
+            if (this.state.activeIndex === 0) {
+              this.setState({
+                activeIndex: this.props.photos.length - 1,
+              });
+            } else {
+              this.setState({ activeIndex: this.state.activeIndex - 1 });
+            }
+          }}
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => {
+            if (this.state.activeIndex === this.props.photos.length - 1) {
+              this.setState({
+                activeIndex: 0,
+              });
+            } else {
+              this.setState({ activeIndex: this.state.activeIndex + 1 });
+            }
+          }}
+        >
+          Next
+        </button>
       </div>
     );
   }
